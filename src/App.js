508269a4242f7c7885cb93dd6f0f0fae824e8228
lizafalko <lizafloyd2@gmail.com>
2020-07-29 12:07:01 +0300

    added new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Cart from './components/Cart'
+import React from 'react';
+import Banner from './Banner';
+import About from './About';
 
-class App extends Component {
-  render() {
-    return (
-       <BrowserRouter>
-            <div className="App">
-            
-              <Navbar/>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/cart" component={Cart}/>
-                  </Switch>
-             </div>
-       </BrowserRouter>
-      
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <About />
+      <Banner isPhoneDefined={true} fullPurchase="50 000" installmentPurchase="2 900" beginnersPurchase="29 000" beginnersNumber="20" />
+      <About />
+      <Banner fullPurchase="50 000" installmentPurchase="2 900" beginnersPurchase="29 000" beginnersNumber="20" />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
+import './fonts/Roboto.ttf'
+import './fonts/Robotobold.ttf'
 import App from './App';
-import './index.css'
-import cartReducer from './components/reducers/cartReducer';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(cartReducer);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
+serviceWorker.unregister();
